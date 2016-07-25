e065eaff85139186478a20b8d06377c4fbe2c54c
Brendan Koral <brendanhkoral@gmail.com>
2016-07-24 20:44:55 -0500

    Add working navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdToolbar } from '@angular2-material/toolbar';
+import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+    MD_CARD_DIRECTIVES,
+    MD_BUTTON_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MdToolbar,
+    MdIcon],
+  providers: [MdIconRegistry]
 })
+
 export class AppComponent {
   title = 'app works!';
 }
